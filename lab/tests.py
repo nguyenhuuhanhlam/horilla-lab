de729a1ec27dfc702e997c335e19e1fd1e098281
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2024-09-27 17:19:54 +0700

    ...office

---
+++ lab/tests.py
@@ -0,0 +1,10 @@
+"""test cases"""
+
+import datetime
+from django.test import TestCase
+from payroll.methods.methods import get_working_days
+
+
+# Create your tests here.
+class HelloLab(TestCase):
+    print()
